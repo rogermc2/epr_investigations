59297bc91d9f74c37a55613b22d4199b933d752a
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-05-06 15:37:57 +1000

    Update

--- violation_of_bell_inequality/R/test.R
+++ violation_of_bell_inequality/R/test.R
@@ -4,5 +4,13 @@ data <- as.numeric (read.csv(paste0(directory, "alice_timetags/OEM_0.csv"), head
 print(paste("Is vector", is.vector(data)))
 print(paste("number of items", length(data)))
 
+print(summary(data))
+
 result.mean <- mean(data)
 print (paste("Mean:", result.mean))
+
+result.var <- var(data)
+print (paste("Variance:", result.var))
+
+df_data = as.data.frame(data)
+print(summary(df_data))
