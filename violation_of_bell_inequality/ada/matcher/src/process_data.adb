5f0977beaffe80f0911a032b6da67ce500b5f8f5
rogermc2 <rogermc@iinet.net.au>
2025-05-11 21:25:38 +1000

    Fixing Find_Closest

--- violation_of_bell_inequality/ada/matcher/src/process_data.adb
+++ violation_of_bell_inequality/ada/matcher/src/process_data.adb
@@ -1,7 +1,6 @@
-
 with Ada.Directories; use Ada.Directories;
-with Ada.Exceptions; use Ada.Exceptions;
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Exceptions;  use Ada.Exceptions;
+with Ada.Text_IO;     use Ada.Text_IO;
 
 with Types; use Types;
 
@@ -9,8 +8,8 @@ package body Process_Data is
 
    procedure Load_Data (CSV_Data : String; Data : out String19_List) is
       use String19_Package;
-      File_ID  : File_Type;
-      aLine    : String_19;
+      File_ID : File_Type;
+      aLine   : String_19;
    begin
       Open (File_ID, In_File, CSV_Data);
       while not End_Of_File (File_ID) loop
@@ -32,51 +31,55 @@ package body Process_Data is
       B_Data       : String19_List;
 
       procedure Find_Closest (A_Curs : String19_Package.Cursor) is
-         A_Index  : constant Extended_Index:= To_Index (A_Curs);
-         A_Val    : constant Double := Double'Value (Element (A_Curs));
-         B_Size   : constant Integer := Integer (Length (B_Data));
-         B_Index  : Extended_Index := A_Index;
+         A_Index  : constant Extended_Index := To_Index (A_Curs);
+         A_Val    : constant Double         := Double'Value (Element (A_Curs));
+         B_Size   : constant Integer        := Integer (Length (B_Data));
+         B_Index  : Extended_Index          := A_Index;
          B_Val    : Double;
-         Min_Diff : Double := Double'Safe_Last;
+         Min_Diff : Double                  := Double'Safe_Last;
          Diff     : Double;
-         Inc      : Integer range -1 .. 1 := 0;
-         Done     : Boolean := False;
+         Inc      : Integer range -1 .. 1   := 0;
+         Done     : Boolean                 := False;
       begin
          if B_Index < B_Size - 1 then
             B_Val := Double'Value (Element (B_Data, B_Index));
-            Diff := abs (B_Val - A_Val);
-
-            if  B_Val < A_Val then
-               Inc := -1;
-            else
-               Inc := 1;
-            end if;
-
-            if Inc = 0 then
+            Done := B_Val = A_Val;
+            if not Done then
+               Diff := abs (B_Val - A_Val);
                Min_Diff := Diff;
-            else
-               while B_Index < B_Size -1 and then not Done loop
+               if B_Val < A_Val then
+                  Inc := -1;
+               else
+                  Inc := 1;
+               end if;
+
+               while B_Index < B_Size - 1 and then not Done loop
                   B_Index := B_Index + Inc;
-                  B_Val := Double'Value (Element (B_Data, B_Index));
-                  Diff := abs (B_Val - A_Val);
-                  Done := Diff >= Min_Diff;
-                  if not Done then
+                  B_Val   := Double'Value (Element (B_Data, B_Index));
+                  Diff    := abs (B_Val - A_Val);
+                  Done    := Diff >= Min_Diff;
+                  if Done then
+                     B_Index := B_Index - Inc;
+                  else
                      Min_Diff := Diff;
                   end if;
                end loop;
-            end if;
-            B_Index := B_Index - Inc;
 
-            Put_Line (Match_ID, Integer'Image (A_Index - 1) & ","  &
-                        Integer'Image (B_Index - 1) & ","  &
-                        Double'Image (Min_Diff));
+            end if;
+
+            Put_Line
+              (Match_ID,
+               Integer'Image (A_Index) & "," & Integer'Image (B_Index) & "," &
+                 Double'Image (Min_Diff));
          end if;
 
       exception
          when Error : others =>
-            Put_Line (Routine_Name & "B_Index, B_Size: " & Integer'Image (B_Size) &
-                        "  " & Integer'Image (B_Size));
-            Put_Line (Routine_Name & "Find_Closest " & Exception_Information (Error));
+            Put_Line
+              (Routine_Name & "B_Index, B_Size: " & Integer'Image (B_Size) &
+                 "  " & Integer'Image (B_Size));
+            Put_Line
+              (Routine_Name & "Find_Closest " & Exception_Information (Error));
             raise;
 
       end Find_Closest;
@@ -90,10 +93,10 @@ package body Process_Data is
 
       Close (Match_ID);
 
-      Put_Line (Routine_Name & "CSV_Match file written to " &
-                  CSV_Match);
-      Put_Line (Routine_Name & "CSV_Match file length: " &
-                  Natural'Image (Natural (Size (CSV_Match))));
+      Put_Line (Routine_Name & "CSV_Match file written to " & CSV_Match);
+      Put_Line
+        (Routine_Name & "CSV_Match file length: " &
+           Natural'Image (Natural (Size (CSV_Match))));
 
    exception
       when Error : others =>
