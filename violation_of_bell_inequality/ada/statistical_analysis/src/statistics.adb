f2ff54740bc22287ad9750c8638aac1cdf88381c
rogermc2 <rogermc@iinet.net.au>
2025-05-16 21:37:11 +1000

    Coding statistics.adb

--- violation_of_bell_inequality/ada/statistical_analysis/src/statistics.adb
+++ violation_of_bell_inequality/ada/statistical_analysis/src/statistics.adb
@@ -1,9 +1,13 @@
 with Estimators;
+with Samples;
 
 procedure Statistics is
 
-  type Data_vector is array (Positive range <>) of Float;
-   package Float_Estimators is new Estimators (Float, Data_vector);
+  type Data_Vector is array (Positive range <>) of Float;
+  type Quantile_Table is array (Positive range <>) of Float;
+   package Float_Estimators is new Estimators (Float, Data_Vector);
+   package Float_Samples is new
+     Samples (Float, Quantile_Table,  use_sub_histogram_index => False);
 
 begin
    null;
