5acb9f82ef8fd224d8ad2786bb4e4dbba48fe4b5
rogermc2 <rogermc@iinet.net.au>
2026-05-28 22:39:47 +1000

    New project check_conversion

---
+++ violation_of_bell_inequality/ada/check_conversion/src/check_conv.adb
@@ -0,0 +1,38 @@
+
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO; use Ada.Text_IO;
+
+--  with Combine_CSVs; use Combine_CSVs;
+with Process_Data; use Process_Data;
+--  with Utils; use Utils;
+
+procedure Check_Conv is
+   Routine_Name : constant String := "Check_Conv ";
+   A_Directory  : constant String :=
+     "../../longdist35/alice_timetags/";
+   B_Directory  : constant String :=
+     "../../longdist35/bob_timetags/";
+   --  AV_Source     : constant String := A_Directory & "longdist35_V.dat";
+   --  BV_Source     : constant String := B_Directory & "longdist35_V.dat";
+   AC_Source     : constant String := A_Directory & "longdist35_C.dat";
+   BC_Source     : constant String := B_Directory & "longdist35_C.dat";
+   --  AV_Target     : constant String := "../A_Photon_Times.csv";
+   --  BV_Target     : constant String := "../B_Photon_Times.csv";
+   AC_Target     : constant String := "../A_OEM.csv";
+   BC_Target     : constant String := "../B_OEM.csv";
+   --  Combined_Data : constant String := "../Long_Dist.csv";
+begin
+   --  Photon_Data (AV_Source, AV_Target);
+   --  Photon_Data (BV_Source, BV_Target);
+
+   OEM_Data (AC_Source, AC_Target);
+   OEM_Data (BC_Source, BC_Target);
+   --  Set stack size:  ulimit -s 64000 to prevent Combine stack overflow
+   --  Combine (AV_Target, BV_Target, AC_Target, BC_Target, Combined_Data, 30000);
+
+exception
+   when Error : others =>
+      Put_Line (Routine_Name & "Exception information:  " &
+                  Exception_Information (Error));
+
+end Check_Conv;
---
+++ violation_of_bell_inequality/ada/check_conversion/src/combine_data.ads
@@ -0,0 +1,12 @@
+
+with Types; use Types;
+
+package Combine_Data is
+
+   procedure Load_Photon_Data (Data_File : String;
+                               Data      : out String21_Array);
+   procedure Load_OEM_Data (Data_File : String;
+                            Data      : out String4_Array);
+   procedure Save_Data (Data_File : String; Data : String53_Array);
+
+end Combine_Data;
