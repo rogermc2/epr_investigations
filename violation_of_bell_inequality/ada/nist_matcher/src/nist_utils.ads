c18d8335c07d409a2bdfaed0f6b4467e2e4a3678
rogermc2 <rogermc@iinet.net.au>
2026-06-30 17:17:38 +1000

    New package nist_utils

---
+++ violation_of_bell_inequality/ada/nist_matcher/src/nist_utils.ads
@@ -0,0 +1,8 @@
+
+with Types; use Types;
+
+package  NIST_Utils is
+
+ procedure Align_Timing_Data (A_Data, B_Data : in out Setting_Time_Vector);
+
+end NIST_Utils;
--- violation_of_bell_inequality/ada/nist_matcher/src/process_sync_data.adb
+++ violation_of_bell_inequality/ada/nist_matcher/src/process_sync_data.adb
@@ -4,65 +4,13 @@ with Ada.Exceptions;  use Ada.Exceptions;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with Histogram;
+with NIST_Utils; use NIST_Utils;
 
 package body Process_Sync_Data is
 
    procedure Load_Sync_Data (CSV_Data : String;
        Sync_Data_A, Sync_Data_B : in out Setting_Time_Vector);
    procedure Save_Match_List (File_Name : String; Pairs : Match_List);
-
-   procedure Align_Timing_Data (A_Data, B_Data : in out Setting_Time_Vector) is
-      use Setting_Time_Package;
-      Routine_Name : constant String := "Process_Sync_Data.Align_Timing_Data ";
-      A_Curs       : Cursor := A_Data.First;
-      B_Curs       : Cursor := B_Data.First;
-      A_Item       : Setting_Time_Record := Element (A_Data.First);
-      B_Item       : Setting_Time_Record := Element (B_Data.First);
-      Delta_Time   : constant Double_Natural :=
-                         abs (A_Item.Time - B_Item.Time);
-      A_Gt_B       : constant Boolean := A_Item.Time >= B_Item.Time;
-      Offset       : Double_Natural;
-      begin
-         if A_Gt_B then
-            Offset := B_Item.Time - 1;
-         else
-            Offset := A_Item.Time - 1;
-         end if;
-         Put_Line (Routine_Name & "Delta_Time: " &
-                   Double_Natural'Image (Delta_Time) &
-                   ", Offset: " & Double_Natural'Image (Offset));
-
-         while Has_Element (A_Curs) and then Has_Element (B_Curs) loop
-            A_Item := Element (A_Curs);
-            B_Item := Element (B_Curs);
-            if A_Gt_B then
-               A_Item.Time := A_Item.Time - Delta_Time;
-               A_Data.Replace_Element (A_Curs, A_Item);
-            else
-               B_Item.Time := B_Item.Time - Delta_Time;
-               B_Data.Replace_Element (B_Curs, B_Item);
-            end if;
-            Next (A_Curs);
-            Next (B_Curs);
-         end loop;
-
-         A_Curs := A_Data.First;
-         B_Curs := B_Data.First;
-         while Has_Element (A_Curs) loop
-            A_Item := Element (A_Curs);
-            A_Item.Time := A_Item.Time - Offset;
-            A_Data.Replace_Element (A_Curs, A_Item);
-            Next (A_Curs);
-         end loop;
-
-         while Has_Element (B_Curs) loop
-            B_Item := Element (B_Curs);
-            B_Item.Time := B_Item.Time - Offset;
-            B_Data.Replace_Element (B_Curs, B_Item);
-            Next (B_Curs);
-         end loop;
-
-   end Align_Timing_Data;
 
 procedure Load_Sync_Data (CSV_Data : String;
                   Sync_Data_A, Sync_Data_B : in out Setting_Time_Vector) is
