63280f5b18abbce290732451e531c7ea9addfab4
rogermc2 <rogermc@iinet.net.au>
2026-07-31 10:19:00 +1000

    File cleanup

--- violation_of_bell_inequality/ada/nist/nist_matcher/src/combine_csvs.ads
+++
@@ -1,7 +0,0 @@
-
-package Combine_CSVs is
-
-   procedure Combine (OEM_aa, OEM_ab, OEM_ba, OEM_bb, Combined_CSV : String;
-                      Test_Rows : Natural := 0);
-
-end Combine_CSVs;
--- violation_of_bell_inequality/ada/nist/nist_matcher/src/nist_utils.ads
+++
@@ -1,8 +0,0 @@
-
-with Types; use Types;
-
-package  NIST_Utils is
-
- procedure Align_Timing_Data (A_Data, B_Data : in out Setting_Time_Vector);
-
-end NIST_Utils;
