1999a39b7d0327899e71eeb47047909bdf45c37b
rogermc2 <rogermc@iinet.net.au>
2026-09-18 16:58:18 +1000

    Initial testing

--- violation_of_bell_inequality/ada/nist/nist_statistical_analysis/src/statistical_analysis.adb
+++ violation_of_bell_inequality/ada/nist/nist_statistical_analysis/src/statistical_analysis.adb
@@ -15,24 +15,24 @@ procedure Statistical_Analysis is
    --   package Float_Samples is new
    --     Samples (Float, Quantile_Table,  use_sub_histogram_index => False);
 
-   AB_Dir        : constant String := "../";
-   OEM_00        : constant String := AB_Dir & "OEM_aa.csv";
-   OEM_01        : constant String := AB_Dir & "OEM_ba.csv";
-   OEM_10        : constant String := AB_Dir & "OEM_ab.csv";
-   OEM_11        : constant String := AB_Dir & "OEM_bb.csv";
-   a00_Data      : constant String := AB_Dir & "a00.csv";
-   b00_Data      : constant String := AB_Dir & "b00.csv";
-   a01_Data      : constant String := AB_Dir & "a01.csv";
-   b01_Data      : constant String := AB_Dir & "b01.csv";
-   a10_Data      : constant String := AB_Dir & "a10.csv";
-   b10_Data      : constant String := AB_Dir & "b10.csv";
-   a11_Data      : constant String := AB_Dir & "a11.csv";
-   b11_Data      : constant String := AB_Dir & "b11.csv";
+   AB_Dir        : constant String := "../generated_nist_data/";
+   OEM_00        : constant String := AB_Dir & "aa.csv";
+   OEM_01        : constant String := AB_Dir & "ba.csv";
+   OEM_10        : constant String := AB_Dir & "ab.csv";
+   OEM_11        : constant String := AB_Dir & "bb.csv";
+   --  a00_Data      : constant String := AB_Dir & "a00.csv";
+   --  b00_Data      : constant String := AB_Dir & "b00.csv";
+   --  a01_Data      : constant String := AB_Dir & "a01.csv";
+   --  b01_Data      : constant String := AB_Dir & "b01.csv";
+   --  a10_Data      : constant String := AB_Dir & "a10.csv";
+   --  b10_Data      : constant String := AB_Dir & "b10.csv";
+   --  a11_Data      : constant String := AB_Dir & "a11.csv";
+   --  b11_Data      : constant String := AB_Dir & "b11.csv";
 
-   Detections_00     : constant Sample_Data_List := Get_Detections (OEM_00, a00_Data, b00_data);
-   Detections_01     : constant Sample_Data_List := Get_Detections (OEM_01, a01_Data, b01_data);
-   Detections_10     : constant Sample_Data_List := Get_Detections (OEM_10, a10_Data, b10_data);
-   Detections_11     : constant Sample_Data_List := Get_Detections (OEM_11, a11_Data, b11_data);
+   --  Detections_00     : constant Sample_Data_List := Get_Detections (OEM_00, a00_Data, b00_data);
+   --  Detections_01     : constant Sample_Data_List := Get_Detections (OEM_01, a01_Data, b01_data);
+   --  Detections_10     : constant Sample_Data_List := Get_Detections (OEM_10, a10_Data, b10_data);
+   --  Detections_11     : constant Sample_Data_List := Get_Detections (OEM_11, a11_Data, b11_data);
    Mean_A_00         : Float;
    Mean_A_01         : Float;
    Mean_A_10         : Float;
@@ -48,19 +48,19 @@ procedure Statistical_Analysis is
    Valid_Data        : Sample_Data_List;
    False_Count       : Natural;
 begin
-   Sample_Means (Detections_00, Mean_A_00, Mean_B_00, Mean_AB_00);
-   Sample_Means (Detections_01, Mean_A_01, Mean_B_01, Mean_AB_01);
-   Sample_Means (Detections_10, Mean_A_10, Mean_B_10, Mean_AB_10);
-   Sample_Means (Detections_11, Mean_A_11, Mean_B_11, Mean_AB_11);
+   --  Sample_Means (Detections_00, Mean_A_00, Mean_B_00, Mean_AB_00);
+   --  Sample_Means (Detections_01, Mean_A_01, Mean_B_01, Mean_AB_01);
+   --  Sample_Means (Detections_10, Mean_A_10, Mean_B_10, Mean_AB_10);
+   --  Sample_Means (Detections_11, Mean_A_11, Mean_B_11, Mean_AB_11);
 
-   Print_Statistics ("OEMA_00", Mean_A_00, Mean_B_00, Mean_AB_00,
-                     Detections_00, Det_A, Det_B);
-   Print_Statistics ("OEMA_01", Mean_A_01, Mean_B_01, Mean_AB_01,
-                     Detections_01, Det_A, Det_B);
-   Print_Statistics ("OEMA_10", Mean_A_10, Mean_B_10, Mean_AB_10,
-                     Detections_10, Det_A, Det_B);
-   Print_Statistics ("OEMA_11", Mean_A_11, Mean_B_11, Mean_AB_11,
-                     Detections_11, Det_A, Det_B);
+   --  Print_Statistics ("OEMA_00", Mean_A_00, Mean_B_00, Mean_AB_00,
+   --                    Detections_00, Det_A, Det_B);
+   --  Print_Statistics ("OEMA_01", Mean_A_01, Mean_B_01, Mean_AB_01,
+   --                    Detections_01, Det_A, Det_B);
+   --  Print_Statistics ("OEMA_10", Mean_A_10, Mean_B_10, Mean_AB_10,
+   --                    Detections_10, Det_A, Det_B);
+   --  Print_Statistics ("OEMA_11", Mean_A_11, Mean_B_11, Mean_AB_11,
+   --                    Detections_11, Det_A, Det_B);
 
    Put_Line ("Overall A Sample_Mean: " &
                Float'Image
@@ -77,15 +77,15 @@ begin
    Put_Line ("- a.b,  b = a + 45 deg.: " & Float'Image (-Cos (Pi / 4.0)));
    New_Line;
 
-   Put_Line ("Number of AB00 detections: " &
-               Integer'Image (Integer (Length (Detections_00))));
-   Put_Line ("Number of AB01 detections: " &
-               Integer'Image (Integer (Length (Detections_01))));
-   Put_Line ("Number of AB10 detections: " &
-               Integer'Image (Integer (Length (Detections_10))));
-   Put_Line ("Number of AB11 detections: " &
-               Integer'Image (Integer (Length (Detections_11))));
-   New_Line;
+   --  Put_Line ("Number of AB00 detections: " &
+   --              Integer'Image (Integer (Length (Detections_00))));
+   --  Put_Line ("Number of AB01 detections: " &
+   --              Integer'Image (Integer (Length (Detections_01))));
+   --  Put_Line ("Number of AB10 detections: " &
+   --              Integer'Image (Integer (Length (Detections_10))));
+   --  Put_Line ("Number of AB11 detections: " &
+   --              Integer'Image (Integer (Length (Detections_11))));
+   --  New_Line;
 
    Valid_Data := False_Positives (OEM_00, False_Count);
    Put_Line ("AB_00 false positives: " & Integer'Image (False_Count));
